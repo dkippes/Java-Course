fa48ed4f4c27e91116a2df5eb90baf573a3d3aef
dkippes <kippes.diego@gmail.com>
2022-01-26 19:50:22 -0300

    Tipo String

--- 2-Variables/HolaMundo/src/PrimitivosBoolean.java
+++
@@ -1,8 +0,0 @@
-public class PrimitivosBoolean {
-    public static void main(String[] args) {
-        boolean datoLogico = false; // Boolean.FALSE
-        System.out.println("datoLogico = " + datoLogico);
-
-        // >, <, ==, <=, >=
-    }
-}
