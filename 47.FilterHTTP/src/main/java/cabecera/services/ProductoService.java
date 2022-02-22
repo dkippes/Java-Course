4f1a4fff9095e903ec464f79813d062a168c8655
dkippes <kippes.diego@gmail.com>
2022-02-22 01:12:42 -0300

    Filter http

---
+++ 47.FilterHTTP/src/main/java/cabecera/services/ProductoService.java
@@ -0,0 +1,11 @@
+package cabecera.services;
+
+import cabecera.models.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoService {
+    List<Producto> listar();
+    Optional<Producto> porId(Long id);
+}
