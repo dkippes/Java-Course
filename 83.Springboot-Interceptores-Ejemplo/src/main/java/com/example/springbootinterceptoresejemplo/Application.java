64d87b81a608bee9f5bbe4466df933a17f7eeae7
dkippes <kippes.diego@gmail.com>
2022-03-16 00:08:19 -0300

    Handler Interceptors

---
+++ 83.Springboot-Interceptores-Ejemplo/src/main/java/com/example/springbootinterceptoresejemplo/Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootinterceptoresejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
